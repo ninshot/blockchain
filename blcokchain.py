db985d8888a94556c93be718215fb4c5692802b6
ninshot <140782230+ninshot@users.noreply.github.com>
2025-06-16 14:48:03 -0600

    addition of block and new transaction code

--- blcokchain.py
+++ blcokchain.py
@@ -1,17 +1,47 @@
 """This python file contains the blockchain class to create new blocks transactions and do hashing of the blocks"""
+from datetime import time
+
 
 class BlockChain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    #A Method used for creation of a new block and adds it to the chain
-    def new_block(self):
-        pass
 
-    #A method used for creation of new transaction and add them to a list
-    def new_transaction(self):
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Creates a new block and adds it to the chain
+        :param proof: <int> The proof given by the proof of work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous block
+        :return: <dict> New block
+        """
+        block={
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined block
+        :param sender: <str> sender's address'
+        :param recipient: <str> recipient's address'
+        :param amount: <int> amount of money
+        :return: <int> index of the  block which holds the new transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
 
     # a method used for hashing a block
     @staticmethod
