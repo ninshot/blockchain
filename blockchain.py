814aa3ebad0cd356f0828b39b037c1029613b646
ninshot <140782230+ninshot@users.noreply.github.com>
2025-06-17 02:10:50 -0600

    addition of mining and new transaction methods

--- blockchain.py
+++ blockchain.py
@@ -3,6 +3,7 @@ import hashlib
 import json
 from cgitb import reset
 from time import time
+from urllib import request
 from uuid import uuid4
 from flask import Flask, jsonify
 from textwrap import dedent
@@ -104,11 +105,41 @@ blockchain = BlockChain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We will mine a new block"
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    #Rewareded one coin for mining and sender is 0 which basically the mine itself
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+    response = {
+        'message': 'New Block Forged',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['POST'])
 def transactions():
-    return "Addition of new Transaction"
+    values = request.get_json()
+    #Check that the required values are present in the POST'ed Data
+    required = ['sender', 'recipient', 'amount']
+    if not all (k in values for k in required):
+        return 'Missing values', 400
+
+    #Creation of new transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to block {index}'}
+
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
